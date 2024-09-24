adfec63b34b4e96b937c27ff42c1a84eeda9e572
vjy-07 <gvijaycrl@gmail.com>
2024-09-24 18:15:15 +0530

    vscode commit

--- Courses.py
+++ Courses.py
@@ -61,3 +61,5 @@ interview_videos = ['https://youtu.be/Tt08KmFfIYQ','https://youtu.be/KukmClH1KoA
                     'https://youtu.be/7_aAicmPB3A','https://youtu.be/1mHjMNZZvFo',
                     'https://youtu.be/WfdtKbAJOmE','https://youtu.be/wFbU185CvDU'
                     'https://youtu.be/wFbU185CvDU','https://youtu.be/TZ3C_syg9Ow']
+
+#vscode
